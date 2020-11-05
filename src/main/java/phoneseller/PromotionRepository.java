d064cf40d4ac8ec5a3070db8c8262cd57a3c3d83
eunjeeheo <phone82ejh@gmail.com>
2020-11-05 20:01:41 +0900

    set polyglot

--- src/main/java/phoneseller/PromotionRepository.java
+++ src/main/java/phoneseller/PromotionRepository.java
@@ -2,7 +2,10 @@ package phoneseller;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.Optional;
+
 public interface PromotionRepository extends PagingAndSortingRepository<Promotion, Long>{
 
+    Optional<Promotion> findByOrderId(Long orderId);
 
 }
